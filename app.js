21cfc4c2ec56f634d6d07b6c6c99ddcf6c05ef7f
akzReekoh <achilles.dinglasan@p3ople4u.com>
2016-03-08 16:27:39 +0800

    REEKOH-293
    -Updated travis.yml
    -Updated app.js to accept array input
    -Bumped version
    -Updated test suite to test array input

--- app.js
+++ app.js
@@ -3,80 +3,91 @@
 var platform = require('./platform'),
     isPlainObject = require('lodash.isplainobject'),
     isEmpty = require('lodash.isempty'),
+    isArray = require('lodash.isarray'),
+    async = require('async'),
 	snsClient, config;
+
+let sendData = (data) => {
+    var params = {
+        Message: 'STRING_VALUE',
+        MessageAttributes: {
+            data: {
+                DataType: 'String',
+                StringValue: ''
+            }
+        },
+        Subject: 'STRING_VALUE',
+        TargetArn: 'STRING_VALUE',
+        TopicArn: 'STRING_VALUE'
+    };
+
+    if(isEmpty(data.message)) {
+        params.Message = config.message;
+        params.MessageAttributes.data.StringValue = config.message;
+    }
+    else {
+        params.Message = data.message;
+        params.MessageAttributes.data.StringValue = data.message;
+    }
+
+    if(isEmpty(data.subject)){
+        if(isEmpty(config.subject))
+            delete params.Subject;
+        else
+            params.Subject = config.subject;
+    }
+    else
+        params.Subject = data.subject;
+
+    if(isEmpty(data.target_arn)){
+        if(isEmpty(config.target_arn))
+            delete params.targetArn;
+        else {
+            params.TargetArn = config.target_arn;
+            delete params.TopicArn;
+        }
+    }
+    else {
+        params.TargetArn = data.target_arn;
+        delete params.TopicArn;
+    }
+
+    if(isEmpty(data.topic_arn)) {
+        params.TopicArn = config.topic_arn;
+        delete params.TargetArn;
+    }
+    else {
+        params.TopicArn = data.topic_arn;
+        delete params.TargetArn;
+    }
+
+    snsClient.publish(params, function(error, response) {
+        if(error){
+            console.error(error);
+            platform.handleException(error);
+        }
+        else{
+            platform.log(JSON.stringify({
+                title: 'AWS SNS message sent.',
+                data: {
+                    Data: params
+                }
+            }));
+        }
+    });
+};
 
 platform.on('data', function (data) {
     if(isPlainObject(data)){
-        var params = {
-            Message: 'STRING_VALUE',
-            MessageAttributes: {
-                data: {
-                    DataType: 'String',
-                    StringValue: ''
-                }
-            },
-            Subject: 'STRING_VALUE',
-            TargetArn: 'STRING_VALUE',
-            TopicArn: 'STRING_VALUE'
-        };
-
-        if(isEmpty(data.message)) {
-            params.Message = config.message;
-            params.MessageAttributes.data.StringValue = config.message;
-        }
-        else {
-            params.Message = data.message;
-            params.MessageAttributes.data.StringValue = data.message;
-        }
-
-        if(isEmpty(data.subject)){
-            if(isEmpty(config.subject))
-                delete params.Subject;
-            else
-                params.Subject = config.subject;
-        }
-        else
-            params.Subject = data.subject;
-
-        if(isEmpty(data.target_arn)){
-            if(isEmpty(config.target_arn))
-                delete params.targetArn;
-            else {
-                params.TargetArn = config.target_arn;
-                delete params.TopicArn;
-            }
-        }
-        else {
-            params.TargetArn = data.target_arn;
-            delete params.TopicArn;
-        }
-
-        if(isEmpty(data.topic_arn)) {
-            params.TopicArn = config.topic_arn;
-            delete params.TargetArn;
-        }
-        else {
-            params.TopicArn = data.topic_arn;
-            delete params.TargetArn;
-        }
-
-        snsClient.publish(params, function(error, response) {
-            if(error){
-                console.error(error);
-                platform.handleException(error);
-            }
-            else{
-                platform.log(JSON.stringify({
-                    title: 'AWS SNS message sent.',
-                    data: {
-                        Data: params
-                    }
-                }));
-            }
+        sendData(data);
+    }
+    else if(isArray(data)){
+        async.each(data, (datum) => {
+            sendData(datum);
         });
     }
     else
-        platform.handleException(new Error('Invalid data received. Must be a valid JSON Object. Data ' + data));
+        platform.handleException(new Error('Invalid data received. Must be a valid Array/JSON Object. Data ' + data));
 });
 
 platform.once('close', function () {
